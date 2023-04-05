d4c18f980d91775984af658fe9dcf96f764742a2
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-05 22:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-//your code here
-// Get all the elements that store the prices of each item
-const prices = document.querySelectorAll('[data-ns-test="prices"]');
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
 
-// Initialize a variable to store the total price
-let totalPrice = 0;
+const getSum = () => {
+//Add your code here
+	let prices = document.querySelectorAll('.price');
+	let sum = 0;
+	prices.forEach(price => {
+		sum += parseFloat(price.textContent);
+	});
+	document.getElementById('table').innerHTML += `
+	   <tr id="ans">
+          <td></td>
+          <td>${sum}</td>
+        </tr>`
+};
 
-// Loop through each price element and add its value to the total price
-for (let i = 0; i < prices.length; i++) {
-  totalPrice += parseFloat(prices[i].innerText);
-}
-
-// Create a new row for displaying the total price
-const newRow = document.createElement('tr');
-const newCell = document.createElement('td');
-
-// Set the data attribute for identifying the new cell
-newCell.setAttribute('data-ns-test', 'grandTotal');
-
-// Set the text content of the new cell to the total price
-newCell.textContent = totalPrice.toFixed(2);
-
-// Append the new cell to the new row
-newRow.appendChild(newCell);
-
-// Append the new row to the table
-const table = document.querySelector('table');
-table.appendChild(newRow);
+getSumBtn.addEventListener("click", getSum)
